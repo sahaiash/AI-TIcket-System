3f4d3756719e01317598fe4198cade1116501b99
sahaiash <sahaiashutoshvinayak@gmail.com>
2025-09-11 02:52:46 +0530

    Complete Frontend Setup and UI Components

---
+++ ai-ticket-frontend/src/pages/ticket.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ticket() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ticket
---
+++ ai-ticket-frontend/src/pages/tickets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function tickets() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default tickets
